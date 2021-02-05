6c4027044c5cf1643f92c750601ccf1198e84c8f
KrishnaSharma206 <74817351+KrishnaSharma206@users.noreply.github.com>
2021-02-05 19:14:17 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,6 +6,7 @@ function setup() {
   weight = random(400, 1500)
   car = createSprite(500, 200, 50, 50);
   wall = createSprite(1500, 200, 60, height/2);
+  wall.shapeColor = color(80, 80, 80);s
 }
 
 function draw() {
